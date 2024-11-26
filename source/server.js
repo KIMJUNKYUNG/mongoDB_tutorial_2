ebd256373dcd09d700975c49791b3c94ef1ef457
KIMJUNKYUNG <kimkj112@naver.com>
2024-11-26 10:42:13 +0800

    URL port 80

--- source/server.js
+++ source/server.js
@@ -12,9 +12,11 @@ const { generateFakeData } = require('../faker2')
 
 const server = async () => {    // async함수 형태로 형성하기
     try {   // async를 쓸땐 try-catch를 사용하는 것이 정석
-        const { MONGO_URI } = process.env
+        const { MONGO_URI, PORT } = process.env
 
         if (!MONGO_URI) throw new Error('MONGO_URI is required')
+        if (!PORT) throw new Error('PORT is required')
+
         await mongoose.connect(MONGO_URI)       // server를 열기전에 DB 연결을 기다리기
         //mongoose.set('debug', true)             // mongoose의 실제 쿼리들을 확인 가능
         console.log('MongoDB connected')
@@ -25,8 +27,8 @@ const server = async () => {    // async함수 형태로 형성하기
         app.use('/blog', blogRouter)
         app.use('/blog/:blogId([0-9a-fA-F]{24})/comment', commentRouter)
 
-        app.listen(3000, async function () {              // Server ON
-            console.log('server listening on port 3000')
+        app.listen(PORT, async function () {              // Server ON
+            console.log(`server listening on port ${PORT}`)
             // for (let i = 0; i < 20; i++) {
             //await generateFakeData(10, 2, 10)
             // }
